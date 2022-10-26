f2721d7c2e7b0fe17e298c2ce3aa214756454ded
Joshua Hayes <josh.hayes121@icloud.com>
2022-10-26 14:13:29 -0400

    fixed spacing

--- src/main/java/hueHome.java
+++ src/main/java/hueHome.java
@@ -14,16 +14,16 @@ public class hueHome {
         System.out.println("Hello world");
 
         // Get a room or a zone -- returns Optional.empty() if the room does not exist, but
-    // let's assume we know for a fact it exists and can do the .get() right away:
+        // let's assume we know for a fact it exists and can do the .get() right away:
         final Room living_room = hue.getRoomByName("Living room").get();
 
-    // Turn the lights on, make them pink:
+        // Turn the lights on, make them pink:
         living_room.setState(State.builder().color(Color.of(java.awt.Color.PINK)).on());
 
-    // Make the entire room dimly lit:
+         // Make the entire room dimly lit:
         living_room.setBrightness(10);
 
-    // Turn one of the lights green. This also demonstrates the proper use of Optionals:
+         // Turn one of the lights green. This also demonstrates the proper use of Optionals:
         final Optional<Light> light = living_room.getLightByName("Black Lamp");
         light.ifPresent(l -> l.setState(State.builder().color(Color.of(java.awt.Color.GREEN.getRGB())).keepCurrentState()));
 
